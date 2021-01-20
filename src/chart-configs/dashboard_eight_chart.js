887e308f1afbd632dbf294191582af6d72316706
Sarbaz770 <48790817+Sarbaz770@users.noreply.github.com>
2021-01-21 00:25:50 +0530

    Add files via upload

---
+++ src/chart-configs/dashboard_eight_chart.js
@@ -0,0 +1,433 @@
+
+
+var eight_chart_today = {
+  "chart": {
+       showBorder: "0",
+    showCanvasBorder: "0",
+    showAlternateHGridColor: "0",
+    bgColor: "#1D1B41",
+    bgAlpha: "0",
+    canvasBgAlpha: "0",
+    baseFontSize: "13",
+    baseFont: "Nunito Sans",
+    showValues: "0",
+    toolTipPadding: "7",
+    toolTipBorderRadius: "3",
+    toolTipBorderAlpha: "30",
+    tooltipBorderThickness: "0.7",
+    usePlotGradientColor: "0",
+    yAxisMinValue: "0",
+    yAxisMaxValue: "50",
+    plotFillAlpha: "10",
+    drawAnchors: "1",
+    anchorRadius: "5",
+    anchorBorderThickness: "1.9",
+    showPlotBorder: "0",
+    showToolTip: "1",
+    canvasTopMargin: "75",
+    canvasBottomMargin: "75",
+    canvasLeftMargin: "30",
+    canvasRightMargin: "30",
+    labelDisplay: "ROTATE",
+    yAxisName: "kWh",
+    transposeAnimation: "1"
+    },
+    "categories": [
+        {
+            "category": [
+                {
+                    "label": "Mon"
+                },
+                {
+                    "label": "Tue"
+                },
+                {
+                    "label": "Wed"
+                },
+                {
+                    "vline": "true",
+                    "lineposition": "0",
+                    "color": "#6baa01",
+                    "labelHAlign": "center",
+                    "labelPosition": "0",
+                    "label": "National holiday",
+                    "dashed": "1"
+                },
+                {
+                    "label": "Thu"
+                },
+                {
+                    "label": "Fri"
+                },
+                {
+                    "label": "Sat"
+                },
+                {
+                    "label": "Sun"
+                }
+            ]
+        }
+    ],
+    "dataset": [
+        {
+            "seriesname": "Water Chiller",
+            "data": [
+                {
+                    "value": "15123"
+                },
+                {
+                    "value": "14233"
+                },
+                {
+                    "value": "25507"
+                },
+                {
+                    "value": "9110"
+                },
+                {
+                    "value": "15529"
+                },
+                {
+                    "value": "20803"
+                },
+                {
+                    "value": "19202"
+                }
+            ]
+        },
+        {
+            "seriesname": "Brine Chiller",
+            "data": [
+                {
+                    "value": "13400"
+                },
+                {
+                    "value": "12800"
+                },
+                {
+                    "value": "22800"
+                },
+                {
+                    "value": "12400"
+                },
+                {
+                    "value": "15800"
+                },
+                {
+                    "value": "19800"
+                },
+                {
+                    "value": "21800"
+                }
+            ]
+        }
+    ],
+    "trendlines": [
+        {
+            "line": [
+                {
+                    "startvalue": "17022",
+                    "color": "#6baa01",
+                    "valueOnRight": "1",
+                    "displayvalue": "Average"
+                }
+            ]
+        }
+    ]
+};
+
+
+
+
+var eight_chart_month = {
+  "chart": {
+       showBorder: "0",
+    showCanvasBorder: "0",
+    showAlternateHGridColor: "0",
+    bgColor: "#1D1B41",
+    bgAlpha: "0",
+    canvasBgAlpha: "0",
+    baseFontSize: "13",
+    baseFont: "Nunito Sans",
+    showValues: "0",
+    toolTipPadding: "7",
+    toolTipBorderRadius: "3",
+    toolTipBorderAlpha: "30",
+    tooltipBorderThickness: "0.7",
+    usePlotGradientColor: "0",
+    yAxisMinValue: "0",
+    yAxisMaxValue: "50",
+    plotFillAlpha: "10",
+    drawAnchors: "1",
+    anchorRadius: "5",
+    anchorBorderThickness: "1.9",
+    showPlotBorder: "0",
+    showToolTip: "1",
+    canvasTopMargin: "75",
+    canvasBottomMargin: "75",
+    canvasLeftMargin: "30",
+    canvasRightMargin: "30",
+    labelDisplay: "ROTATE",
+    yAxisName: "kWh",
+    transposeAnimation: "1"
+    },
+    "categories": [
+        {
+            "category": [
+                {
+                    "label": "Mon"
+                },
+                {
+                    "label": "Tue"
+                },
+                {
+                    "label": "Wed"
+                },
+                {
+                    "vline": "true",
+                    "lineposition": "0",
+                    "color": "#6baa01",
+                    "labelHAlign": "center",
+                    "labelPosition": "0",
+                    "label": "National holiday",
+                    "dashed": "1"
+                },
+                {
+                    "label": "Thu"
+                },
+                {
+                    "label": "Fri"
+                },
+                {
+                    "label": "Sat"
+                },
+                {
+                    "label": "Sun"
+                }
+            ]
+        }
+    ],
+    "dataset": [
+        {
+            "seriesname": "Water Chiller",
+            "data": [
+                {
+                    "value": "15123"
+                },
+                {
+                    "value": "14233"
+                },
+                {
+                    "value": "25507"
+                },
+                {
+                    "value": "9110"
+                },
+                {
+                    "value": "15529"
+                },
+                {
+                    "value": "20803"
+                },
+                {
+                    "value": "19202"
+                }
+            ]
+        },
+        {
+            "seriesname": "Brine Chiller",
+            "data": [
+                {
+                    "value": "25400"
+                },
+                {
+                    "value": "22800"
+                },
+                {
+                    "value": "17800"
+                },
+                {
+                    "value": "13400"
+                },
+                {
+                    "value": "17800"
+                },
+                {
+                    "value": "18800"
+                },
+                {
+                    "value": "12800"
+                }
+            ]
+        }
+    ],
+    "trendlines": [
+        {
+            "line": [
+                {
+                    "startvalue": "17022",
+                    "color": "#6baa01",
+                    "valueOnRight": "1",
+                    "displayvalue": "Average"
+                }
+            ]
+        }
+    ]
+};
+
+
+// year chart
+
+
+var eight_chart_year = {
+  "chart": {
+       showBorder: "0",
+    showCanvasBorder: "0",
+    showAlternateHGridColor: "0",
+    bgColor: "#1D1B41",
+    bgAlpha: "0",
+    canvasBgAlpha: "0",
+    baseFontSize: "13",
+    baseFont: "Nunito Sans",
+    showValues: "0",
+    toolTipPadding: "7",
+    toolTipBorderRadius: "3",
+    toolTipBorderAlpha: "30",
+    tooltipBorderThickness: "0.7",
+    usePlotGradientColor: "0",
+    yAxisMinValue: "0",
+    yAxisMaxValue: "50",
+    plotFillAlpha: "10",
+    drawAnchors: "1",
+    anchorRadius: "5",
+    anchorBorderThickness: "1.9",
+    showPlotBorder: "0",
+    showToolTip: "1",
+    canvasTopMargin: "75",
+    canvasBottomMargin: "75",
+    canvasLeftMargin: "30",
+    canvasRightMargin: "30",
+    labelDisplay: "ROTATE",
+    yAxisName: "kWh",
+    transposeAnimation: "1"
+    },
+    "categories": [
+        {
+            "category": [
+                {
+                    "label": "Mon"
+                },
+                {
+                    "label": "Tue"
+                },
+                {
+                    "label": "Wed"
+                },
+                {
+                    "vline": "true",
+                    "lineposition": "0",
+                    "color": "#6baa01",
+                    "labelHAlign": "center",
+                    "labelPosition": "0",
+                    "label": "National holiday",
+                    "dashed": "1"
+                },
+                {
+                    "label": "Thu"
+                },
+                {
+                    "label": "Fri"
+                },
+                {
+                    "label": "Sat"
+                },
+                {
+                    "label": "Sun"
+                }
+            ]
+        }
+    ],
+    "dataset": [
+        {
+            "seriesname": "Water Chiller",
+            "data": [
+                {
+                    "value": "14132"
+                },
+                {
+                    "value": "11233"
+                },
+                {
+                    "value": "20507"
+                },
+                {
+                    "value": "7110"
+                },
+                {
+                    "value": "17529"
+                },
+                {
+                    "value": "23803"
+                },
+                {
+                    "value": "12202"
+                }
+            ]
+        },
+        {
+            "seriesname": "Brine Chiller",
+            "data": [
+                {
+                    "value": "13400"
+                },
+                {
+                    "value": "12800"
+                },
+                {
+                    "value": "22800"
+                },
+                {
+                    "value": "12400"
+                },
+                {
+                    "value": "15800"
+                },
+                {
+                    "value": "19800"
+                },
+                {
+                    "value": "21800"
+                }
+            ]
+        }
+    ],
+    "trendlines": [
+        {
+            "line": [
+                {
+                    "startvalue": "17022",
+                    "color": "#6baa01",
+                    "valueOnRight": "1",
+                    "displayvalue": "Average"
+                }
+            ]
+        }
+    ]
+};
+
+
+
+var chartConfigs8 = {
+    type: "msline",
+    className: "fc-mssplinearea",
+    id: "mychart8",
+    dataFormat: "JSON",
+    width: "100%",
+    height: "300",
+    dataSource: eight_chart_month
+};
+
+export default chartConfigs8;
+export { eight_chart_today };
+export { eight_chart_month };
+export { eight_chart_year };
